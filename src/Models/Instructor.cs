5f67e53d2698f6263e800e3bf086a6ea0f347641
Jordan Williams <jordanwilliams288@gmail.com>
2018-09-26 10:59:32 -0500

    finished exercises

--- src/Models/Instructor.cs
+++ src/Models/Instructor.cs
@@ -1,15 +1,38 @@
 using Dapper;
 using Microsoft.Data.Sqlite;
+using System.ComponentModel.DataAnnotations;
 
 namespace Workforce.Models
 {
     public class Instructor
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Slack handle")]
         public string SlackHandle { get; set; }
+
+        [Required]
         public string Specialty { get; set; }
+
+        [Display(Name = "Instructor Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public int CohortId { get; set; }
         public Cohort Cohort { get; set; }
     }
 }
